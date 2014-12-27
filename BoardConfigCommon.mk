ae580e6414f3f2a191aee6f3f4e181fbb762c08f
christer12 <christer.bjor@gmail.com>
2014-12-27 02:22:13 +0100

    hlte-common: add common ril
    Conflicts:
    
    	system_prop.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,6 +57,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 BOARD_HARDWARE_CLASS += device/samsung/hlte-common/cmhw
 
+# Ril
+BOARD_RIL_CLASS := ../../../device/samsung/hlte-common/ril
+
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
 BOARD_USES_LEGACY_MMAP := true
